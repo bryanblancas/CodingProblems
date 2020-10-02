33d28dd0501d9e728964f03599adf8d63fbcb766
bryanblancas <orionmuneycanica@gmail.com>
2020-10-01 20:08:14 -0500

    Corrections to isSubstring function

--- isSubstring.py
+++ isSubstring.py
@@ -3,19 +3,25 @@ def isSubstring(s, t):
 	size_s = len(s)
 	idx_t = 0
 
-	if(size_s < size_t):
+	if(size_s < size_t or size_t == 0 or size_s == 0):
 		return False
 
-	for i in s:
-		if(i == t[idx_t]):
+	i = 0
+	while i < size_s:
+		if(s[i] == t[idx_t]):
 			idx_t += 1
 		else:
 			idx_t = 0
+			i -= 1
+
+		i += 1
+
 		if(idx_t >= size_t):
 			return True
+
 	return False
 
 if __name__ == '__main__':
-	s = "bryan"
-	t = "t"
+	s = "brybryannnnn"
+	t = "bryan"
 	print(isSubstring(s,t))
